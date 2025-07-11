31ddf3ab06305346f3759286f4a989a1eb633cff
Edun Omatseye <edunomatseye@yahoo.com>
2025-07-11 18:45:44 +0100

    More test on Data and time from bun

--- __test/index.test.ts
+++ __test/index.test.ts
@@ -6,6 +6,7 @@ import {
   test,
   beforeEach,
   afterEach,
+  jest,
 } from "bun:test";
 
 describe("Bun server", () => {
@@ -22,4 +23,27 @@ describe("Bun server", () => {
     expect(random.mock.calls).toEqual([[]]);
     expect(random.mock.results).toEqual([{ type: "return", value: 42 }]);
   });
+});
+
+test("just like in jest", () => {
+  jest.useFakeTimers();
+  jest.setSystemTime(new Date("2020-01-01T00:00:00.000Z"));
+  expect(new Date().getFullYear()).toBe(2020);
+  jest.useRealTimers();
+  expect(new Date().getFullYear()).toBeGreaterThan(2020);
+});
+
+test("unlike in jest", () => {
+  const OriginalDate = Date;
+  jest.useFakeTimers();
+  if (typeof Bun === "undefined") {
+    // In Jest, the Date constructor changes
+    // That can cause all sorts of bugs because suddenly Date !== Date before the test.
+    expect(Date).not.toBe(OriginalDate);
+    expect(Date.now).not.toBe(OriginalDate.now);
+  } else {
+    // In bun:test, Date constructor does not change when you useFakeTimers
+    expect(Date).toBe(OriginalDate);
+    expect(Date.now).toBe(OriginalDate.now);
+  }
 });
