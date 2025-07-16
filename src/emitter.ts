1765a6592ea2d66bc87720131c4a643e3ff6760c
Edun Omatseye <edunomatseye@yahoo.com>
2025-07-16 22:15:01 +0100

    chore: custom event class added

---
+++ src/emitter.ts
@@ -0,0 +1,48 @@
+class EventEmitter extends EventTarget {
+  constructor(private name: string) {
+    super();
+    this.name = name;
+  }
+
+  postMessage(message: string) {
+    const msgEvent = new CustomEvent("ms", {
+      detail: { message, deta: { value: 450 } },
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(msgEvent);
+  }
+
+  triggerAlert(data: { message: string; deta: { value: number } }) {
+    const msgEvent = new CustomEvent("alert", {
+      detail: data,
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(msgEvent);
+  }
+}
+
+class CustomEventEmitter {
+  private events: Map<string, ((event: CustomEvent) => void)[]>;
+  constructor() {
+    this.events = new Map();
+  }
+
+  on(eventName: string, callback: (event: CustomEvent) => void) {
+    if (!this.events.has(eventName)) {
+      this.events.set(eventName, []);
+    }
+    this.events.get(eventName)?.push(callback);
+    return () => this.off(eventName, callback);
+  }
+
+  off(eventName: string, callback: (event: CustomEvent) => void) {
+    return this.events.get(eventName)?.filter((cb) => cb !== callback);
+  }
+
+  emit(eventName: string, ...data: any[]) {
+    const event = new CustomEvent(eventName, { detail: data });
+    this.events.get(eventName)?.forEach((cb) => cb(event));
+  }
+}
