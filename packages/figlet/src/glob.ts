623875e03f0b6863c0a2c027289a244cdd92416e
Edun Omatseye <edunomatseye@yahoo.com>
2025-07-23 16:30:19 +0100

    refactor: update node:fs to glob package

--- packages/figlet/src/glob.ts
+++ packages/figlet/src/glob.ts
@@ -1,25 +1,24 @@
-import { globSync } from "node:fs";
-import { writeFileSync, mkdirSync } from "node:fs";
-import { randomUUIDv7 } from "bun";
-import { stdout } from "node:process";
+import { globSync } from 'glob';
+import { writeFileSync, mkdirSync } from 'node:fs';
+import { randomUUIDv7 } from 'bun';
 
-mkdirSync("a", { recursive: true });
-mkdirSync("b", { recursive: true });
+mkdirSync('a', { recursive: true });
+mkdirSync('b', { recursive: true });
 
-writeFileSync("a/apple.ts", "console.log('apple');");
-writeFileSync("b/banana.ts", "console.log('banana');");
-writeFileSync("b/cherry.js", "console.log('cherry');");
-writeFileSync("b/cherry.ts", "console.log('cherry');");
+writeFileSync('a/apple.ts', "console.log('apple');");
+writeFileSync('b/banana.ts', "console.log('banana');");
+writeFileSync('b/cherry.js', "console.log('cherry');");
+writeFileSync('b/cherry.ts', "console.log('cherry');");
 
 // The 'exclude' option should be an array of glob patterns, not a function.
-const files = globSync("**/*.ts", {
-  exclude: ["node_modules/**"],
+const files = globSync('**/*.ts', {
+  ignore: ['node_modules/**'],
 });
 console.log(files);
-console.log(randomUUIDv7("hex"));
+console.log(randomUUIDv7('hex'));
 
-const buf = Buffer.from("Hello, world!".repeat(100));
-const encoded = new TextEncoder().encode("Hello, world!".repeat(100));
+const buf = Buffer.from('Hello, world!'.repeat(100));
+const encoded = new TextEncoder().encode('Hello, world!'.repeat(100));
 encoded.reverse();
 const compressed = Bun.gzipSync(encoded);
 console.log(buf.byteLength);
@@ -28,5 +27,5 @@ console.log(compressed.length);
 const decompressed = Bun.gunzipSync(compressed);
 //console.log(new TextDecoder().decode(decompressed));
 
-const appleFruit = Bun.file("a/apple.ts");
+const appleFruit = Bun.file('a/apple.ts');
 await Bun.write(Bun.stdout, await appleFruit.text());
