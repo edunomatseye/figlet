8df2282d0c0e2bc0fe019dfea84b11dffe5657ef
Edun Omatseye <edunomatseye@yahoo.com>
2025-07-28 14:47:00 +0100

    chore: add drizzle query to user script

--- packages/auth/script/user.ts
+++ packages/auth/script/user.ts
@@ -1,17 +1,31 @@
 import { PrismaClient } from '../generated/prisma/client.ts';
-const prisma = new PrismaClient();
+import { drizzle } from 'drizzle-orm/prisma/sqlite';
+import { User } from '../../../packages/auth/drizzle/schema';
+
+const prisma = new PrismaClient().$extends(drizzle());
 
 async function main() {
   const user = await prisma.profile.create({
     data: {
-      name: 'Omare',
-      email: 'omare@prisma.io',
+      name: 'Jimoh',
+      email: 'jimoh@prisma.io',
     },
   });
   console.log(user);
 
   const users = await prisma.profile.findMany();
   console.log(users);
+
+  const drizzle = await prisma.$drizzle.run(`
+    SELECT * FROM "Profile";
+  `);
+  console.log(drizzle);
+  const drizzleUsers = await prisma.$drizzle.run('SELECT * FROM "Profile"');
+  console.log(drizzleUsers);
+  const drizzleUser = await prisma.$drizzle.run(
+    `SELECT * FROM "Profile" WHERE id = ${User.id}`
+  );
+  console.log(drizzleUser);
 }
 
 main()
