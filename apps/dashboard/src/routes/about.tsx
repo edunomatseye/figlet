cdf18a5929f92cef0d985ee0adbbdb9a632fd74d
Edun Omatseye <edunomatseye@yahoo.com>
2025-08-01 19:36:44 +0100

    chore: add a button that navigates to a new route

--- apps/dashboard/src/routes/about.tsx
+++ apps/dashboard/src/routes/about.tsx
@@ -1,9 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, getRouteApi } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/about')({
   component: About,
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  const routeApi = getRouteApi('/about');
+  const navigate = routeApi.useNavigate();
+  return (
+    <div className="p-2">
+      Hello from About! <br />
+      <button
+        style={{
+          marginTop: '10px',
+          color: 'blue',
+          cursor: 'pointer',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          padding: '10px 20px',
+          borderRadius: '5px',
+          border: '1px solid blue',
+          backgroundColor: 'white',
+          transition: 'all 0.3s ease',
+          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+        }}
+        onClick={() => navigate({ to: '/table', params: { postId: '1' } })}
+      >
+        Go to Table
+      </button>
+    </div>
+  );
 }
